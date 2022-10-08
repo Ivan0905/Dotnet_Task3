69337ac1fed9ccc7cc94009dbb022733793541c4
Ivan <ia.farafonov@yandex.ru>
2022-10-08 19:28:57 +0300

    решена задача 21

--- Program.cs
+++ Program.cs
@@ -1,25 +1,18 @@
 ﻿// Домашнее задание №3
 
-//Задача 19
-//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-Console.Write($"Введите пятизначное число: ");
-int num = int.Parse(Console.ReadLine()!);
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+// ними в 3D пространстве.
 
-while ((num<10000)||(num>99999))
-{
-    Console.Write($"Введено не пятизначное число. Введите повторно: ");
-    num = int.Parse(Console.ReadLine()!);
-}
-int num1 = num / 10000;
-int num2 = num % 10000 / 1000;
-int num4 = num % 100 / 10;
-int num5 = num % 10;
+Console.WriteLine("Введите координаты первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine()!);
+double y1 = Convert.ToDouble(Console.ReadLine()!);
+double z1 = Convert.ToDouble(Console.ReadLine()!);
+Console.WriteLine("Введите координаты второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine()!);
+double y2 = Convert.ToDouble(Console.ReadLine()!);
+double z2 = Convert.ToDouble(Console.ReadLine()!);
 
-if ((num1 == num5) && (num2 == num4))
-{
-    Console.WriteLine($"Число {num} является палиндромом");
-}
-else
-{
-    Console.WriteLine($"Число {num}  НЕ является палиндромом");
-}
+double len = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)+Math.Pow ((z2-z1),2));
+
+Console.WriteLine($"Расстояние между точками равняется {len:f3}");
